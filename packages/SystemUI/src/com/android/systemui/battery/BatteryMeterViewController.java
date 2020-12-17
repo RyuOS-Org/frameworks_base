0f80fadb94056a1e49a187ed37b5f3d060257e4f
Jyotiraditya Panda <jyotiraditya@aospa.co>
2020-12-17 19:40:32 +0000

    SystemUI: Show Bolt icon while charging
    [@techyminati] Adapt to T
    [alibei] Adapted to 12L
    [SakthivelNadar] Adapted to T QPR3
    [sajidshahriar72543] Adapted to V
    
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Signed-off-by: alibei <sebastiankotzias83@gmail.com>
    Signed-off-by: SakthivelNadar <s2234nadar@gmail.com>
    Signed-off-by: sajidshahriar72543 <sazidshahriar39@gmail.com>

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterViewController.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterViewController.java
@@ -171,6 +171,7 @@ public class BatteryMeterViewController extends ViewController<BatteryMeterView>
         mUserTracker.addCallback(mUserChangedCallback, new HandlerExecutor(mMainHandler));
 
         mView.updateShowPercent();
+        mView.updateBatteryMeterVisibility();
     }
 
     @Override
