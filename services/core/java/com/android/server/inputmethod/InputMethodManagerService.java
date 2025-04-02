Variation-LOS/android_frameworks_base

fixup! Introduce high touch polling rate feature control
Change-Id: Id89757988182607ec19f5470dd8ac788282f0140

services/core/java/com/android/server/inputmethod/InputMethodManagerService.java
@@ -486,6 +486,12 @@ public final class InputMethodManagerService implements IInputMethodManagerImpl.
         void registerContentObserverForAllUsers() {
             ContentResolver resolver = mContext.getContentResolver();
             if (mLineageHardware.isSupported(
+                    LineageHardwareManager.FEATURE_HIGH_TOUCH_POLLING_RATE)) {
+                resolver.registerContentObserverAsUser(LineageSettings.System.getUriFor(
+                        LineageSettings.System.HIGH_TOUCH_POLLING_RATE_ENABLE),
+                        false, this, UserHandle.ALL);
+            }
+            if (mLineageHardware.isSupported(
                     LineageHardwareManager.FEATURE_HIGH_TOUCH_SENSITIVITY)) {
                 resolver.registerContentObserverAsUser(LineageSettings.System.getUriFor(
                         LineageSettings.System.HIGH_TOUCH_SENSITIVITY_ENABLE),
@@ -505,6 +511,8 @@ public final class InputMethodManagerService implements IInputMethodManagerImpl.
         }
 
         private void onChangeInternal(@NonNull Uri uri, @UserIdInt int userId) {
+            final Uri highTouchPollingRateUri = LineageSettings.System.getUriFor(
+                    LineageSettings.System.HIGH_TOUCH_POLLING_RATE_ENABLE);
             final Uri touchSensitivityUri = LineageSettings.System.getUriFor(
                     LineageSettings.System.HIGH_TOUCH_SENSITIVITY_ENABLE);
             final Uri touchHoveringUri = LineageSettings.Secure.getUriFor(
@@ -513,7 +521,9 @@ public final class InputMethodManagerService implements IInputMethodManagerImpl.
                 if (!mConcurrentMultiUserModeEnabled && mCurrentImeUserId != userId) {
                     return;
                 }
-                if (touchSensitivityUri.equals(uri)) {
+                if (highTouchPollingRateUri.equals(uri)) {
+                    updateTouchPollingRate();
+                } else if (touchSensitivityUri.equals(uri)) {
                     updateTouchSensitivity();
                 } else if (touchHoveringUri.equals(uri)) {
                     updateTouchHovering();

RyuOS-Org/frameworks_base

services/core/java/com/android/server/inputmethod/InputMethodManagerService.java
@@ -488,6 +488,12 @@ public final class InputMethodManagerService implements IInputMethodManagerImpl.
         void registerContentObserverForAllUsers() {
             ContentResolver resolver = mContext.getContentResolver();
             if (mLineageHardware.isSupported(
+                    LineageHardwareManager.FEATURE_HIGH_TOUCH_POLLING_RATE)) {
+                resolver.registerContentObserverAsUser(Settings.System.getUriFor(
+                        Settings.System.HIGH_TOUCH_POLLING_RATE_ENABLE),
+                        false, this, UserHandle.ALL);
+            }
+            if (mLineageHardware.isSupported(
                     LineageHardwareManager.FEATURE_HIGH_TOUCH_SENSITIVITY)) {
                 resolver.registerContentObserverAsUser(Settings.System.getUriFor(
                         Settings.System.HIGH_TOUCH_SENSITIVITY_ENABLE),
@@ -507,6 +513,8 @@ public final class InputMethodManagerService implements IInputMethodManagerImpl.
         }
 
         private void onChangeInternal(@NonNull Uri uri, @UserIdInt int userId) {
+            final Uri highTouchPollingRateUri = Settings.System.getUriFor(
+                    Settings.System.HIGH_TOUCH_POLLING_RATE_ENABLE);
             final Uri touchSensitivityUri = Settings.System.getUriFor(
                     Settings.System.HIGH_TOUCH_SENSITIVITY_ENABLE);
             final Uri touchHoveringUri = Settings.Secure.getUriFor(
@@ -515,7 +523,9 @@ public final class InputMethodManagerService implements IInputMethodManagerImpl.
                 if (!mConcurrentMultiUserModeEnabled && mCurrentImeUserId != userId) {
                     return;
                 }
-                if (touchSensitivityUri.equals(uri)) {
+                if (highTouchPollingRateUri.equals(uri)) {
+                    updateTouchPollingRate();
+                } else if (touchSensitivityUri.equals(uri)) {
                     updateTouchSensitivity();
                 } else if (touchHoveringUri.equals(uri)) {
                     updateTouchHovering();